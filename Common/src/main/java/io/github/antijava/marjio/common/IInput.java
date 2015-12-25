9e0c2c26f3a9f44ad76aa5547f976347e787f827
firejox <firejox@gmail.com>
2015-12-25 17:53:55 +0800

    Complete Input part Implement KeyCodeConverter Define KeyInfo Define Status Define Event

--- Common/src/main/java/io/github/antijava/marjio/common/IInput.java
+++ Common/src/main/java/io/github/antijava/marjio/common/IInput.java
@@ -1,11 +1,19 @@
 package io.github.antijava.marjio.common;
 
+import java.util.List;
+
 /**
  * Created by fntsr on 2015/12/23.
  */
 public interface IInput {
-    boolean isPressing();
-    boolean isPressed();
-    boolean isReleased();
-    boolean isTrigger();
+    void update();
+
+    boolean isPressing(Key key);
+    boolean isPressed(Key key);
+    boolean isReleased(Key key);
+    boolean isTrigger(Key key);
+
+    List<Status> getStatuses();
+
+    void triggerEvent(Event evt);
 }
